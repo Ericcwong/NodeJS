8004b122aadb1e217176904e2571e1fb1283afc2
Eric Wong <ericwong12@live.com>
2020-01-27 12:21:26 -0700

    Added questions to prompt users what they want but hot dang, that is one big npm package

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 const questions = [
-  
+  {
+      type: "input",
+      name: "name",
+      message: "What is your Github username?"
+  },
+  {
+      type: "input",
+      name: "color",
+      message: "What color would you like for your Github PDF?"
+  }
 ];
-
+function promptUser(){
+    return inquirer.prompt(questions);
+}
+promptUser();
 function writeToFile(fileName, data) {
  
 }
